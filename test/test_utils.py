d36632f4f49451f6cb7927c6915c3dd8133720f1
Laay Trivedi <laaytrivedi@gmail.com>
2020-08-10 04:35:54 -0500

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,50 +4,34 @@ import pytest
 import pandas as pd
 import numpy as np
 
-path_cal_housing = "file://data/california_housing_test.csv"
-df_pd = pd.read_csv('data/california_housing_test.csv')
+object_name1 = ObjectStorageDataset(f"gcs://gs://storage_bucket01/BicycleWeather.csv", batch_size= 200, iterations = 20)
 
 
 
-class TestObjectShape(object):
 
-  ### test dimensions
-    def test_dimensions(self):
-        object_name = ObjectStorageDataset(path_cal_housing)
-        expected = len(df_pd)
-        actual = object_name.dataset_size
-        message = "object length {0} and actual object length {1} doesn't match".format(expected, actual)
-        assert actual == expected, message
 
 class TestBatchSize(object):
 
-  ### test when batch_size is less than or equal to zero
-    def test_when_input_less_than_zero(self):
-        batch_input = 10
-        object_name = ObjectStorageDataset(path_cal_housing, batch_size= batch_input)
-        actual = object_name.batch_size
+
+    def test_when_input_less_than_zero(self):      
+        actual = object_name1.batch_size
+        expected = 200
+        max = 1340
         message = "The batch size must be specified as a positive (greater than 0) integer"  
+        message1 = "object_name.batch_size should return the int {0}, but it actually returned {1}".format(expected, actual)
+        message2 = "object_name.batch_size can not exceed more than the size of dataset"
         assert actual > 0, message
+        assert type(object_name1.batch_size) is int, "The batch size must be an integer"
+        assert actual == expected, message1
+        assert actual <= max, message2 
+               
 
+class TestIterations(object):       
 
-  ### test when batch size is not integer
-    def test_when_input_not_int(self):
-          batch_input = 15
-          object_name = ObjectStorageDataset(path_cal_housing, batch_size= batch_input)
-          assert type(object_name.batch_size) is int, "The batch size must be an integer"
-
-  
-  ### test when batch size input is None
-    def test_when_input_is_None(self):
-        batch_input = None
-        expected = 3000
-        object_name = ObjectStorageDataset(path_cal_housing, batch_size= batch_input)
-        actual = object_name.batch_size
-        message = "object_name.batch_size should return the int {0}, but it actually returned {1}".format(expected, actual)    
-        assert actual == expected, message
-
-  ### test when batch size input is more than the size of dataset
-    def test_when_input_is_more_than_size_of_dataset(self):
-        batch_input = 10
-        max = 3000
-        object_name = ObjectStorageDataset(path_cal_housing, batch_size= batch_input)
+   def test_iterations(self):
+      actual = 20
+      expected = object_name1.iterations
+      message = "object_name.iterations should match with entered number"
+      message1 = "object_name.iterations should be integer"
+      assert actual == expected, message
+      assert type(actual) is int, message1

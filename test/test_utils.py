d938997a40fac02315d55b64ddc3e273d0f4e649
Laay Trivedi <laaytrivedi@gmail.com>
2020-08-12 04:02:22 -0500

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -7,8 +7,8 @@ import numpy as np
 object_name1 = ObjectStorageDataset(f"gcs://gs://storage_bucket01/BicycleWeather.csv", batch_size = 20)
 
 
-
-batch1 = next(iter(DataLoader(object_name1)))
+batch = iter(DataLoader(object_name1))
+batch1 = next(batch)
 
 
 class TestObjectShape(object):

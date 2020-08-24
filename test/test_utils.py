ce1fc38d6c72d0c43618cfebe897cbe3d01ab2e2
Laay Trivedi <laaytrivedi@gmail.com>
2020-08-24 18:06:20 -0500

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,7 +4,7 @@ import pytest
 import pandas as pd
 import numpy as np
 
-object_name1 = ObjectStorageDataset(f"gcs://gs://cloud-training-demos/taxifare/large/taxi-train*.csv", batch_size = 2000, worker =4,iterations = 200,  eager_load_batches= False, dtype = 'float64')
+object_name1 = ObjectStorageDataset(f"gcs://gs://storage_bucket01/BicycleWeather.csv", storage_options = {'anon' : False }, batch_size= 20, iterations = 20,  eager_load_batches= False, dtype = 'int64')
 
 
 class TestBatchSize(object):
@@ -13,20 +13,20 @@ class TestBatchSize(object):
     def test_input_batchsize(self):      
         actual = object_name1.batch_size
         expected = 2000
-        max = 2156815
+        max = object_name1.dataset_size
         message = "The batch size must be specified as a positive (greater than 0) integer"  
         message1 = "object_name.batch_size should return the int {0}, but it actually returned {1}".format(expected, actual)
         message2 = "object_name.batch_size can not exceed more than the size of dataset"
         assert actual > 0, message
-        assert type(object_name1.batch_size) is int, "The batch size must be an integer"
+        assert type(object_name.batch_size) is int, "The batch size must be an integer"
         assert actual == expected, message1
         assert actual <= max, message2 
-
+               
 
 class TestIterations(object):       
 
    def test_iterations(self):
-      actual = 200
+      actual = 20
       expected = object_name1.iterations
       message = "object_name.iterations should match with entered number"
       message1 = "object_name.iterations should be integer"
@@ -38,7 +38,7 @@ class TestObjectDataType(object):
 
   ### test default data type - it should be float64
     def test_default_dtype(self):
-        expected = 'float64'
+        expected = 'int64'
         actual = str(object_name1.dtype)
         message = "expected object dtype {0} and actual object dtype {1} doesn't match".format(expected, actual)
         assert actual == expected, message

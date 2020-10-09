338d38e1f69f3f3d27d5b43d5fda5015e52ec86d
Laay Trivedi <laaytrivedi@gmail.com>
2020-10-08 23:20:47 -0500

    Update test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,7 +4,15 @@ import pytest
 import pandas as pd
 import numpy as np
 
-object_name1 = ObjectStorageDataset(f"gcs://gs://storage_bucket01/BicycleWeather.csv", storage_options = {'anon' : False }, batch_size= 20, iterations = 20,  eager_load_batches= False, dtype = 'int64')
+object_name1 = ObjectStorageDataset(f"gcs://gs://cloud-training-demos/taxifare/large/taxi-train*.csv",  
+                                       storage_options = {'anon' : True }, 
+                                       dtype='float32',
+                                       batch_size = 16384, 
+                                       eager_load_batches=False)
+
+
+batch1 = next(iter(DataLoader(object_name1)))
+
 
 
 class TestBatchSize(object):
@@ -12,8 +20,8 @@ class TestBatchSize(object):
 
     def test_input_batchsize(self):      
         actual = object_name1.batch_size
-        expected = 20
-        max = 1340
+        expected = 16384
+        max = 16384
         message = "The batch size must be specified as a positive (greater than 0) integer"  
         message1 = "object_name1.batch_size should return the int {0}, but it actually returned {1}".format(expected, actual)
         message2 = "object_name1.batch_size can not exceed more than the size of dataset"
@@ -26,19 +34,19 @@ class TestBatchSize(object):
 class TestIterations(object):       
 
    def test_iterations(self):
-      actual = 20
+      actual = 'nan'
       expected = object_name1.iterations
       message = "object_name.iterations should match with entered number"
       message1 = "object_name.iterations should be integer"
-      assert actual == expected, message
-      assert type(actual) is int, message1
+      assert actual == str(expected), message
+      assert type(actual) is str, message1
 
                
 class TestObjectDataType(object):
 
   ### test default data type - it should be float64
     def test_default_dtype(self):
-        expected = 'int64'
+        expected = 'float32'
         actual = str(object_name1.dtype)
         message = "expected object dtype {0} and actual object dtype {1} doesn't match".format(expected, actual)
         assert actual == expected, message
